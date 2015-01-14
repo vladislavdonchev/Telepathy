6049b9642953e2f6b9df1e933839c95df68ae83b
Vladislav Donchev <vladislav.donchev@circles.bz>
2015-01-14 15:29:59 +0200

    Reorganized project structure, referenced libsuperuser from as separate project module with own repository.

---
+++ src/main/java/com/koushikdutta/async/callback/ListenCallback.java
@@ -0,0 +1,11 @@
+package com.koushikdutta.async.callback;
+
+import com.koushikdutta.async.AsyncServerSocket;
+import com.koushikdutta.async.AsyncSocket;
+
+
+public interface ListenCallback extends CompletedCallback {
+    public void onAccepted(AsyncSocket socket);
+
+    public void onListening(AsyncServerSocket socket);
+}
---
+++ src/main/java/com/koushikdutta/async/http/AsyncHttpResponse.java
@@ -0,0 +1,18 @@
+package com.koushikdutta.async.http;
+
+import com.koushikdutta.async.AsyncSocket;
+import com.koushikdutta.async.DataEmitter;
+
+public interface AsyncHttpResponse extends DataEmitter {
+    public String protocol();
+
+    public String message();
+
+    public int code();
+
+    public Headers headers();
+
+    public AsyncSocket detachSocket();
+
+    public AsyncHttpRequest getRequest();
+}
