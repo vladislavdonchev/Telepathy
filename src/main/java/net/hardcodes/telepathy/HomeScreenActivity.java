90a931052ebd86f0d0a5a23b5c5fb079b2b0fcc9
Vladi <vladislav.donchev@circles.bz>
2015-02-04 17:40:10 +0200

    Added options for automatic deployment and removal of the application from the system. This closes issue #9.

--- src/main/java/net/hardcodes/telepathy/HomeScreenActivity.java
+++ src/main/java/net/hardcodes/telepathy/HomeScreenActivity.java
@@ -1,18 +1,36 @@
 package net.hardcodes.telepathy;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import net.hardcodes.telepathy.tools.ShellCommandExecutor;
+
+public class HomeScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean isAppDeployedInSystem = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("appDeployed", false);
+        if (!isAppDeployedInSystem) {
+            if (ShellCommandExecutor.isSUAvailable()) {
+                InstallUninstallDialog installUninstallDialog = new InstallUninstallDialog(this);
+                installUninstallDialog.setTitle("Application Deployment");
+                installUninstallDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Install", installUninstallDialog);
+                installUninstallDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Uninstall", installUninstallDialog);
+                installUninstallDialog.show();
+            } else {
+                Toast.makeText(this, "SU not available. This device can only be used as a client.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
@@ -36,11 +54,11 @@ public class MainActivity extends Activity {
     }
 
     public void startClient(View v) {
-        new AddressInputDialog().show(getFragmentManager(), "Remote Control");
+        new ConnectDialog().show(getFragmentManager(), "Remote Control");
     }
 
     public void startServer(View v) {
-        Intent startServerIntent = new Intent(MainActivity.this, RemoteControlService.class);
+        Intent startServerIntent = new Intent(HomeScreenActivity.this, RemoteControlService.class);
         startServerIntent.setAction("START");
         startService(startServerIntent);
         finish();
---
+++ src/main/java/net/hardcodes/telepathy/InstallUninstallDialog.java
@@ -0,0 +1,72 @@
+package net.hardcodes.telepathy;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import net.hardcodes.telepathy.tools.ShellCommandExecutor;
+
+/**
+ * Created by MnQko on 4.2.2015 г..
+ */
+public class InstallUninstallDialog extends AlertDialog implements DialogInterface.OnClickListener {
+
+    private static final String INSTALL_SCRIPT =
+            "sleep 3\n" +
+                    "mount -o rw,remount /system\n" +
+                    "cat %s > /system/priv-app/Telepathy.apk.tmp\n" +
+                    "chmod 644 /system/priv-app/Telepathy.apk.tmp\n" +
+                    "pm uninstall %s\n" +
+                    "mv /system/priv-app/Telepathy.apk.tmp /system/priv-app/Telepathy.apk\n" +
+                    "pm install -r /system/priv-app/Telepathy.apk\n" +
+                    "sleep 5\n" +
+                    "am start -n net.hardcodes.telepathy/.HomeScreenActivity";
+
+    private static final String UNINSTALL_SCRIPT =
+            "mount -o rw,remount /system\n" +
+                    "rm /system/priv-app/Telepathy.apk.tmp\n" +
+                    "rm /data/app/net.hardcodes.*\n" +
+                    "reboot";
+
+    protected InstallUninstallDialog(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                    deployInSystem();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                    uninstall();
+                break;
+        }
+    }
+
+    private void deployInSystem() {
+        Toast.makeText(getContext(), "Installing system service. Please wait...", Toast.LENGTH_LONG).show();
+        ShellCommandExecutor.getInstance().runCommand(String.format(INSTALL_SCRIPT,
+                new String[]{
+                        getContext().getPackageCodePath(),
+                        getContext().getPackageName()
+                }));
+        PreferenceManager.getDefaultSharedPreferences(getContext()).edit().putBoolean("appDeployed", true).commit();
+        gotoHomeScreen();
+    }
+
+    private void gotoHomeScreen() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getContext().startActivity(startMain);
+    }
+
+    private void uninstall() {
+        Toast.makeText(getContext(), "Uninstalling application and rebooting system...", Toast.LENGTH_LONG).show();
+        ShellCommandExecutor.getInstance().runCommand(UNINSTALL_SCRIPT);
+    }
+}
